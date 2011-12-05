e0432c5fba6f64d331033bc983576f308655eff3
Xingzhong <xu.xeno@gmail.com>
2011-12-05 16:44:03 -0500

    well improve the mexopts, now support opencl compile and link in Mac and Linux

--- mexopts.sh
+++ mexopts.sh
@@ -1,27 +1,13 @@
 #
 # mexopts.sh	Shell script for configuring MEX-file creation script,
 #               mex.  These options were tested with the specified compiler.
-#
-# usage:        Do not call this file directly; it is sourced by the
-#               mex shell script.  Modify only if you don't like the
-#               defaults after running mex.  No spaces are allowed
-#               around the '=' in the variable assignment.
-#
-# Note: For the version of system compiler supported with this release,
-#       refer to the Supported and Compatible Compiler List at:
-#       http://www.mathworks.com/support/compilers/current_release/
-#
-#
-# SELECTION_TAGs occur in template option files and are used by MATLAB
-# tools, such as mex and mbuild, to determine the purpose of the contents
-# of an option file. These tags are only interpreted when preceded by '#'
-# and followed by ':'.
-#
-#SELECTION_TAG_MEX_OPT: Template Options file for building MEX-files via the system ANSI compiler
-#
-# Copyright 1984-2008 The MathWorks, Inc.
-# $Revision: 1.78.4.16 $  $Date: 2008/11/04 19:40:11 $
-#----------------------------------------------------------------------------
+# 
+# Modified by Xingzhong
+# this is the mex configuration file 
+# glnxa64 is for Linux 64
+# maci64 is for Macintosh 64
+# Dec. 5 2011
+# -------------------
 #
     TMW_ROOT="$MATLAB"
     MFLAGS=''
@@ -32,74 +18,12 @@
     fi
     case "$Arch" in
         Undetermined)
-#----------------------------------------------------------------------------
-# Change this line if you need to specify the location of the MATLAB
-# root directory.  The script needs to know where to find utility
-# routines so that it can determine the architecture; therefore, this
-# assignment needs to be done while the architecture is still
-# undetermined.
-#----------------------------------------------------------------------------
+
             MATLAB="$MATLAB"
-            ;;
-        glnx86)
-#----------------------------------------------------------------------------
-            RPATH="-Wl,-rpath-link,$TMW_ROOT/bin/$Arch"
-            # StorageVersion: 1.0
-            # CkeyName: GNU C
-            # CkeyManufacturer: GNU
-            # CkeyLanguage: C
-            # CkeyVersion:
-            CC='gcc'
-            CFLAGS='-ansi -D_GNU_SOURCE'
-            CFLAGS="$CFLAGS -fPIC -pthread -m32"
-            CFLAGS="$CFLAGS  -fexceptions"
-            CFLAGS="$CFLAGS -D_FILE_OFFSET_BITS=64" 
-            CLIBS="$RPATH $MLIBS -lm"
-            COPTIMFLAGS='-O -DNDEBUG'
-            CDEBUGFLAGS='-g'
-            CLIBS="$CLIBS -lstdc++"
-#           
-            # C++keyName: GNU C++
-            # C++keyManufacturer: GNU
-            # C++keyLanguage: C++
-            # C++keyVersion: 
-            CXX='g++'
-            CXXFLAGS='-ansi -D_GNU_SOURCE'
-            CXXFLAGS="$CXXFLAGS -D_FILE_OFFSET_BITS=64" 
-            CXXFLAGS="$CXXFLAGS -fPIC -pthread"
-            CXXLIBS="$RPATH $MLIBS -lm"
-            CXXOPTIMFLAGS='-O -DNDEBUG'
-            CXXDEBUGFLAGS='-g'
-#
-#
-            # FortrankeyName: gfortran
-            # FortrankeyManufacturer: GNU
-            # FortrankeyLanguage: Fortran
-            # FortrankeyVersion: 
-            FC='gfortran'
-            FFLAGS='-fexceptions -fbackslash'
-            FFLAGS="$FFLAGS -fPIC"
-            FLIBS="$RPATH $MLIBS -lm"
-            FOPTIMFLAGS='-O'
-            FDEBUGFLAGS='-g'
-#
-            LD="$COMPILER"
-            LDEXTENSION='.mexglx'
-            LDFLAGS="-pthread -shared -m32 -Wl,--version-script,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -Wl,--no-undefined"
-            LDOPTIMFLAGS='-O'
-            LDDEBUGFLAGS='-g'
-#
-            POSTLINK_CMDS=':'
-#----------------------------------------------------------------------------
             ;;
         glnxa64)
 #----------------------------------------------------------------------------
             RPATH="-Wl,-rpath-link,$TMW_ROOT/bin/$Arch"
-            # StorageVersion: 1.0
-            # CkeyName: GNU C
-            # CkeyManufacturer: GNU
-            # CkeyLanguage: C
-            # CkeyVersion:
             CC='gcc'
             CFLAGS='-ansi -D_GNU_SOURCE'
             CFLAGS="$CFLAGS  -fexceptions"
@@ -108,11 +32,7 @@
             COPTIMFLAGS='-O -DNDEBUG'
             CDEBUGFLAGS='-g'
             CLIBS="$CLIBS -lstdc++"
-#
-            # C++keyName: GNU C++
-            # C++keyManufacturer: GNU
-            # C++keyLanguage: C++
-            # C++keyVersion: 
+
             CXX='g++'
             CXXFLAGS='-ansi -D_GNU_SOURCE'
             CXXFLAGS="$CXXFLAGS -fPIC -fno-omit-frame-pointer -pthread"
@@ -120,49 +40,17 @@
             CXXOPTIMFLAGS='-O -DNDEBUG'
             CXXDEBUGFLAGS='-g'
 #
-            # FortrankeyName: gfortran
-            # FortrankeyManufacturer: GNU
-            # FortrankeyLanguage: Fortran
-            # FortrankeyVersion: 
-#
-            FC='gfortran'
-            FFLAGS='-fexceptions -fbackslash'
-            FFLAGS="$FFLAGS -fPIC -fno-omit-frame-pointer"
-            FLIBS="$RPATH $MLIBS -lm"
-            FOPTIMFLAGS='-O'
-            FDEBUGFLAGS='-g'
-#
             LD="$COMPILER"
             LDEXTENSION='.mexa64'
-            LDFLAGS="-pthread -shared -Wl,--version-script,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -Wl,--no-undefined"
+            LDFLAGS="-pthread -shared -Wl,--version-script,$TMW_ROOT/extern/lib/$Arch/$MAPFILE -Wl,--no-undefined -lOpenCL"
             LDOPTIMFLAGS='-O'
             LDDEBUGFLAGS='-g'
 #
             POSTLINK_CMDS=':'
 #----------------------------------------------------------------------------
             ;;
-        sol64)
-#----------------------------------------------------------------------------
-echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh sol64 12
-#----------------------------------------------------------------------------
-            ;;
-        mac)
-#----------------------------------------------------------------------------
-echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh mac 12
-#----------------------------------------------------------------------------
-            ;;
-        maci)
-#----------------------------------------------------------------------------
-echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh maci 12
-#----------------------------------------------------------------------------
-            ;;
         maci64)
 #----------------------------------------------------------------------------
-            # StorageVersion: 1.0
-            # CkeyName: GNU C
-            # CkeyManufacturer: GNU
-            # CkeyLanguage: C
-            # CkeyVersion:
             CC='llvm-gcc'
             SDKROOT='/Developer/SDKs/MacOSX10.6.sdk'
             MACOSX_DEPLOYMENT_TARGET='10.5'
@@ -174,27 +62,12 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh maci 12
             CDEBUGFLAGS='-g'
 #
             CLIBS="$CLIBS -lstdc++"
-            # C++keyName: GNU C++
-            # C++keyManufacturer: GNU
-            # C++keyLanguage: C++
-            # C++keyVersion: 
-            CXX=g++-4.2
+
+            CXX=llvm-g++
             CXXFLAGS="-fno-common -no-cpp-precomp -fexceptions -arch $ARCHS -isysroot $SDKROOT -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
-            CXXLIBS="$MLIBS -lstdc++"
+            CXXLIBS="$MLIBS -lstdc++ -framework OpenCL"
             CXXOPTIMFLAGS='-O2 -DNDEBUG'
             CXXDEBUGFLAGS='-g'
-#
-            # FortrankeyName: GNU Fortran
-            # FortrankeyManufacturer: GNU
-            # FortrankeyLanguage: Fortran
-            # FortrankeyVersion: 
-            FC='gfortran'
-            FFLAGS='-fexceptions -m64 -fbackslash'
-            FC_LIBDIR=`$FC -print-file-name=libgfortran.dylib 2>&1 | sed -n '1s/\/*libgfortran\.dylib//p'`
-            FC_LIBDIR2=`$FC -print-file-name=libgfortranbegin.a 2>&1 | sed -n '1s/\/*libgfortranbegin\.a//p'`
-            FLIBS="$MLIBS -L$FC_LIBDIR -lgfortran -L$FC_LIBDIR2 -lgfortranbegin"
-            FOPTIMFLAGS='-O'
-            FDEBUGFLAGS='-g'
 #
             LD="$CC"
             LDEXTENSION='.mexmaci64'
@@ -207,28 +80,3 @@ echo "Error: Did not imbed 'options.sh' code"; exit 1 #imbed options.sh maci 12
 #----------------------------------------------------------------------------
             ;;
     esac
-#############################################################################
-#
-# Architecture independent lines:
-#
-#     Set and uncomment any lines which will apply to all architectures.
-#
-#----------------------------------------------------------------------------
-#           CC="$CC"
-#           CFLAGS="$CFLAGS"
-#           COPTIMFLAGS="$COPTIMFLAGS"
-#           CDEBUGFLAGS="$CDEBUGFLAGS"
-#           CLIBS="$CLIBS"
-#
-#           FC="$FC"
-#           FFLAGS="$FFLAGS"
-#           FOPTIMFLAGS="$FOPTIMFLAGS"
-#           FDEBUGFLAGS="$FDEBUGFLAGS"
-#           FLIBS="$FLIBS"
-#
-#           LD="$LD"
-#           LDFLAGS="$LDFLAGS"
-#           LDOPTIMFLAGS="$LDOPTIMFLAGS"
-#           LDDEBUGFLAGS="$LDDEBUGFLAGS"
-#----------------------------------------------------------------------------
-#############################################################################
